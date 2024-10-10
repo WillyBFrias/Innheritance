e1e5659693e366d6a54074378eaa123e5c035cf0
gbacio <gbacio@expertdata.com.mx>
2024-10-09 20:12:09 -0600

    Agregar archivos de proyecto.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innheritance
+{
+    class Employee : Human
+    {
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public void SetValues()
+        {
+            Name = "Willy";
+            ID = 1;
+            Gender = 'M';
+            Age = 27;
+        }
+    }
+}
